b6472f41d356a76b6188ef4d577d51f2daf3b818
Alexxx180 <alexx120702@gmail.com>
2024-06-19 18:55:50 +0300

    continued Rauss criteria matlab porting

--- menu/rauss/solutions.py
+++ menu/rauss/solutions.py
@@ -16,14 +16,14 @@ class RaussCriteria:
 
     def __first_rows(self):
         edges: list = [0, 0]
-        for i in range(1, self.count):
+        for i in range(0, self.count):
             self.__set(edges, i)
 
     def __formula(self, row1: list, row2: list, j: int, div: float):
         return row1[0] * row2[j] - row2[0] * row1[j] / div
 
     def __calculate(self, i: int):
-        for j in range(2, self.columns):
+        for j in range(1, self.columns):
             print(i - 2)
             print(self.matrix)
             row2: list = self.matrix[i - 2]
@@ -35,25 +35,25 @@ class RaussCriteria:
     def __main_body(self, rows: int):
         for i in range(2, rows - 1):
             self.__calculate(i)
+        self.matrix[rows][0] = self.matrix[rows - 2][1]
 
     def __xor(self, l1: list, l2: list) -> list:
         return [x ^ y for x, y in zip(l1, l2)]
 
     def __define_poles(self):
         self.RHP: float = 0
-        self.result = not any(self.matrix[:][0] <= 0)
+        self.result = any(self.matrix[:][0] <= 0)
 
         if self.result:
             signs: list = self.matrix[:][0] > 0
             end: int = len(signs)
             self.RHP = sum(self.__xor(signs[0:end - 1], signs[1:end]))
 
+        self.result = not self.result
         self.LHP: float = (self.count - 1) - self.RHP
 
     def study(self):
         self.result = all(self.c) == 0
         self.__first_rows()
-        rows: int = len(self.matrix[0])
-        self.__main_body(rows)
-        self.matrix[rows][0] = self.matrix[rows - 2][1]
+        self.__main_body(len(self.matrix[0]))
         self.__define_poles()
